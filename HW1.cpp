c095554bd7f25890f30370cb9b8e5abf828235a1
riceroll89 <jonathanc89@sbcglobal.net>
2014-09-08 16:20:29 -0700

    HW1

---
+++ HW1.cpp
@@ -0,0 +1,78 @@
+/*       Name: JongHoon Choi
+*       Date: September 6, 2014
+*      Class: CSC 340.01
+* Assignment: Homework 1
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+string stringPara;
+
+int main()
+{
+	cout << "Enter the paragraph string: ";
+	cin >> stringPara >> endl;
+	return 0;
+}
+
+int getFreqLetter()
+{
+	//char temp;
+	//vector < char > ;
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+	int g;
+	int h;
+	int i;
+	int j;
+	int k;
+	int l;
+	int m;
+	int n;
+	int o;
+	int p;
+	int q;
+	int r;
+	int s;
+	int t;
+	int u;
+	int v;
+	int w;
+	int x;
+	int y;
+	int z;
+
+	for (int i = 0; i < stringPara.size(); i++)
+		if (stringPara[i] = 'a') a++;
+		else if (stringPara[i] = 'b') b++;
+		else if (stringPara[i] = 'c') c++;
+		else if (stringPara[i] = 'd') d++;
+		else if (stringPara[i] = 'e') e++;
+		else if (stringPara[i] = 'f') f++;
+		else if (stringPara[i] = 'g') g++;
+		else if (stringPara[i] = 'h') h++;
+		else if (stringPara[i] = 'i') i++;
+		else if (stringPara[i] = 'j') j++;
+		else if (stringPara[i] = 'k') k++;
+		else if (stringPara[i] = 'l') l++;
+		else if (stringPara[i] = 'm') m++;
+		else if (stringPara[i] = 'n') n++;
+		else if (stringPara[i] = 'o') o++;
+		else if (stringPara[i] = 'p') p++;
+		else if (stringPara[i] = 'q') q++;
+		else if (stringPara[i] = 'r') r++;
+		else if (stringPara[i] = 's') t++;
+		else if (stringPara[i] = 'u') u++;
+		else if (stringPara[i] = 'v') v++;
+		else if (stringPara[i] = 'w') w++;
+		else if (stringPara[i] = 'x') x++;
+		else if (stringPara[i] = 'y') y++;
+		else if (stringPara[i] = 'z') z++;
+
+
+
+}
