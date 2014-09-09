651d262f2e6841dca0be7904cf781ac3c39b9028
riceroll89 <jonathanc89@sbcglobal.net>
2014-09-08 22:45:22 -0700

    HW1

--- HW1.cpp
+++ HW1.cpp
@@ -4,196 +4,45 @@
 * Assignment: Homework 1
 */
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
-char *stringPara;
-int getFreqLetter();
-int max;
-
+void getFreqLetter(string stringPara);
 int main()
 {
+	string stringPara;
 	cout << "Enter the paragraph string: ";
-	cin >> *stringPara;
-	getFreqLetter();
-	cout << "Most common letter is used " << max << " times." << endl;
+	cin >> stringPara;
+	getFreqLetter(stringPara);
+
 	return 0;
 }
 
-int getFreqLetter()
+void getFreqLetter(string stringPara)
 {
-	int array[26];
-	char temp;
-	while (temp = *stringPara++){
-		switch (temp)
+//Use 65 - 122 in ASCII for upper and lower case alphbet
+	char letterArray[57];
+	int i;
+	for (i = 0; i < 56; i++)
+		letterArray[i] = 0;
+
+	for (i = 0; i < stringPara.length(); i++)
+	{
+		letterArray[stringPara[i]-65]++;
+	}
+
+	int index;
+	int count = 0;
+	int countLetter = 0;
+	for (index = 0; index < 56; index++)
+	{
+		if (letterArray[index] > count)
 		{
-		case 'A':
-			array[0]++;
-			break;
-		case 'a':
-			array[0]++;
-			break;
-		case 'B':
-			array[1]++;
-			break;
-		case 'b':
-			array[1]++;
-			break;
-		case 'C':
-			array[2]++;
-			break;
-		case 'c':
-			array[2]++;
-			break;
-		case 'D':
-			array[3]++;
-			break;
-		case 'd':
-			array[3]++;
-			break;
-		case 'E':
-			array[4]++;
-			break;
-		case 'e':
-			array[4]++;
-			break;
-		case 'F':
-			array[5]++;
-			break;
-		case 'f':
-			array[5]++;
-			break;
-		case 'G':
-			array[6]++;
-			break;
-		case 'g':
-			array[6]++;
-			break;
-		case 'H':
-			array[7]++;
-			break;
-		case 'h':
-			array[7]++;
-			break;
-		case 'I':
-			array[8]++;
-			break;
-		case 'i':
-			array[8]++;
-			break;
-		case 'J':
-			array[9]++;
-			break;
-		case 'j':
-			array[9]++;
-			break;
-		case 'K':
-			array[10]++;
-			break;
-		case 'k':
-			array[10]++;
-			break;
-		case 'L':
-			array[11]++;
-			break;
-		case 'l':
-			array[11]++;
-			break;
-		case 'M':
-			array[12]++;
-			break;
-		case 'm':
-			array[12]++;
-			break;
-		case 'N':
-			array[13]++;
-			break;
-		case 'n':
-			array[13]++;
-			break;
-		case 'O':
-			array[14]++;
-			break;
-		case 'o':
-			array[14]++;
-			break;
-		case 'P':
-			array[15]++;
-			break;
-		case 'p':
-			array[15]++;
-			break;
-		case 'Q':
-			array[16]++;
-			break;
-		case 'q':
-			array[16]++;
-			break;
-		case 'R':
-			array[17]++;
-			break;
-		case 'r':
-			array[17]++;
-			break;
-		case 'S':
-			array[18]++;
-			break;
-		case 's':
-			array[18]++;
-			break;
-		case 'T':
-			array[19]++;
-			break;
-		case 't':
-			array[19]++;
-			break;
-		case 'U':
-			array[20]++;
-			break;
-		case 'u':
-			array[20]++;
-			break;
-		case 'V':
-			array[21]++;
-			break;
-		case 'v':
-			array[21]++;
-			break;
-		case 'W':
-			array[22]++;
-			break;
-		case 'w':
-			array[22]++;
-			break;
-		case 'X':
-			array[23]++;
-			break;
-		case 'x':
-			array[23]++;
-			break;
-		case 'Y':
-			array[24]++;
-			break;
-		case 'y':
-			array[24]++;
-			break;
-		case 'Z':
-			array[25]++;
-			break;
-		case 'z':
-			array[25]++;
-			break;
-		default:
-			break;
-
-
-
+			count = letterArray[index];
+			countLetter = index + 65;
 		}
 	}
-	int max = 0;
-	for (int i = 0; i < 26; i++)
-	{
-		if (array[i] > max)
-			max = array[i];
-	}
-	return max;
+
+
+
+	cout << "Most common letter, " << (char)countLetter << " is used " << count << " times." << endl;
 }
