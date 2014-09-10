052c41c2ffbc1885086c0a4622b0aaf468b4e8e1
riceroll89 <jonathanc89@sbcglobal.net>
2014-09-10 15:12:16 -0700

    Q1
    CSC340 string organization program

--- Q1.cpp
+++ Q1.cpp
@@ -1,11 +1,10 @@
 /*       Name: JongHoon Choi
-*       Date: September 9, 2014
+*       Date: September 10, 2014
 *      Class: CSC 340.01
 * Assignment: Homework 1
 */
 #include <iostream>
 #include <string>
-#include <locale>
 #include <sstream>
 #include <algorithm>
 using namespace std;
@@ -35,7 +34,7 @@ int main()
 	replace(stringPara.begin(), stringPara.end(), ',', ' ');
 	replace(stringPara.begin(), stringPara.end(), '.', ' ');
 
-	cout << stringPara << endl;
+	//cout << stringPara << endl; //test if string is converted correctly
 	getFreqLetter(stringPara);
 
 	wordFreq wordArray[100];
@@ -52,7 +51,7 @@ int main()
 		int i = 0;
 		while (i < 100)
 		{
-			if (word == wordArray[i].word)
+			if (word == wordArray[i].word) //if word already exists in the array, increment word count by 1
 			{
 				wordArray[i].cnt++;
 				break;
@@ -63,8 +62,8 @@ int main()
 		{
 			for (i = 0; i < 100; i++)
 			{
-				if (wordArray[i].word == "")
-				{
+				if (wordArray[i].word == "") //if the word is not currently in the array, add to the first available
+				{							 //slot, then increment the count by 1
 					wordArray[i].word = word;
 					wordArray[i].cnt++;
 					break;
@@ -72,16 +71,32 @@ int main()
 			}
 		}
 	}
-	//test unique word array
-	for (i = 0; i < 100; i++)
+
+	//sorts the words by most used first
+	int j;
+	int index;
+	int count = 0;
+	string countWord;
+	for (j = 0; j < 100; j++)
 	{
-		cout << wordArray[i].word << " : " << wordArray[i].cnt << endl;
-		if (wordArray[i].word == "")
+		for (i = 0; i < 100; i++)
+		{
+			if (wordArray[i].cnt > count)
+			{
+				count = wordArray[i].cnt;
+				countWord = wordArray[i].word;
+				index = i;
+			}
+		}
+		if (count == 0)
 		{
 			break;
 		}
+		cout << countWord << " : " << count << endl;
+		wordArray[index].cnt = 0;
+		wordArray[index].word = ""; //removes most used word after its printed
+		count = 0; //resets the counter to select the next most used word
 	}
-	return 0;
 }
 
 void getFreqLetter(string stringPara)
